e7513750426f1db14c4d18e1c7be65e1bb9fcab1
jnjaruncts <arun.jothikrishnan@cognizant.com>
2021-10-21 07:24:56 -0700

    Add files via upload

---
+++ BSLConstants.java
@@ -0,0 +1,10 @@
+package com.example.filedemo.utils;
+public class BSLConstants {
+
+
+
+	public static final String  SITE = "^[0-9]+$";
+	public static final String  CAMPAIGN = "^[a-zA-Z0-9]+$";
+	public static final String  EVENTTYPE = "A|ML1|TDB|TDR|SLN|TDT|SLR|SL1|BRN|TDN";
+	public static final String  LOCALE = "^[a-z]{2}_[A-Z]{2}$";
+}
